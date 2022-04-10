e7a2d67b6b10e841c984f1aabb7a269f929e5f3f
Candace Robbins <canrob0522@gmail.com>
2022-04-09 23:18:36 -0500

    started on JS

--- Assets/script.js
+++ Assets/script.js
@@ -7,6 +7,14 @@ let searchInput
 let todayBox
 let forecastBox
 let searchDisplayBox
+let cityName= document.getElementById("weather-searchCity");
+let currentDate= document.querySelector("date");
+let weatherPic= document.getElementById("weather-icon");
+let currentTemp= document.querySelector(".temp");
+let currentWind= document.querySelector(".wind");
+let currentHumidity= document.querySelector(".humidity");
+let currentUV= document.querySelector(".uvIndex");
+
 
 // Add timezone from day.js
 dayjs.extend(window.dayjs_plugin_utc);
@@ -14,21 +22,43 @@ dayjs.extend(window.dayjs_plugin_timezone);
 
 function displaySearchHistory() {
     searchDisplayBox.innerHTML = "";
-}
-var weatherApiUrl = `http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=${apiKey}`
+
+// }
+var weatherApiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=390d66b37dcbd064dba3890d8db84761`;
 
 // call and show api key
-function weatherData(weatherApi) {
+function weatherData(weather) {
     
-    fetch(weatherApi)
-    .then (function(response) {
-        return response.json()
+    fetch(weather)
+        .then (function(response) {
+            console.log(response.status);
+            return response.json()
     })
-    .then(function(data){
-        console.log(data);
-    })
+        .then(function(data){
+            console.log(data);
+    });
 }
+
 weatherData(weatherApiUrl);
+
+function searchCity(cityName) {
+    var weatherURL="http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=" + apiKey;
+
+    fetch(weatherURL)
+        .then(function(response){
+        
+            cityName.innerHTML = response.data.name + "(" + month + "/" + day + "/" + year + ")";
+            currentTemp.innerHTML = "Temperature: " + k2f(response.data.main.temp) + "&176F";
+            console.log(weatherURL);
+        })
+}
+
+searchForm.addEventListener("click", function(){
+    let searchInput = city
+})
+}
+
+
 
 // make request to the one call and show them how to make an API key
 // fetch coordinates
